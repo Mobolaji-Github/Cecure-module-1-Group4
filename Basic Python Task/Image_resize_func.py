b90735761f040431f6c10e5cae2814ad7006a62e
Mobolaji Bada <97799019+Mobolaji-Github@users.noreply.github.com>
2022-09-08 15:54:08 +0100

    Update Image_resize_func.py

--- Basic Python Task/Image_resize_func.py
+++ Basic Python Task/Image_resize_func.py
@@ -1,3 +1,6 @@
+from PIL import Image
+import glob,os
+
 def Image_Resizer(File_path,Image_name,Base_width,Base_height,Resized_Image_name):
     # Then we open the image with 
     im = Image.open(os.path.join(File_path,Image_name))
